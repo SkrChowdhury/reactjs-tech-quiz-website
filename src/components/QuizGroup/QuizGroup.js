1bb5dedea200e38f1b7ad9a6dcc90663a24192f9
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-12 01:58:35 +0600

    final commit

--- src/components/QuizGroup/QuizGroup.js
+++ src/components/QuizGroup/QuizGroup.js
@@ -37,8 +37,8 @@ const QuizGroup = ({ topics }) => {
               for Programmers
             </h2>
             <p className="text-base text-gray-700 md:text-lg">
-              Try ours Quizes to sharpen your knowledge and win awards achieving
-              places among top 50
+              Try out Quizes to sharpen your knowledge and win awards by achieving
+              places among top 50. Happy Quizing !! 
             </p>
           </div>
           <div className="grid  gap-10 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 sm:mx-auto">
