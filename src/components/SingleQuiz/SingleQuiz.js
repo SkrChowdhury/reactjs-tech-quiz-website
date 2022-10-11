9dbde7f1ded5a576116a5e0d56599e326977059d
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-11 19:03:52 +0600

    heroicon added

--- src/components/SingleQuiz/SingleQuiz.js
+++ src/components/SingleQuiz/SingleQuiz.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SingleQuizOptions from '../SingleQuizOptions/SingleQuizOptions';
+import { EyeIcon } from '@heroicons/react/24/solid';
 
 const SingleQuiz = ({ quiz }) => {
   const options = quiz.options;
@@ -10,8 +11,14 @@ const SingleQuiz = ({ quiz }) => {
           <p className="text-lg text-start font-semibold">{quiz.question}</p>
           <div className="grid lg:grid-cols-2 md:grid-cols-1">
             {options.map((option) => (
-              <SingleQuizOptions key={quiz.id} option={option}></SingleQuizOptions>
+              <SingleQuizOptions
+                key={quiz.id}
+                option={option}
+              ></SingleQuizOptions>
             ))}
+          </div>
+          <div className="flex justify-center">
+            <EyeIcon className=" h-8 w-8 text-blue-500"></EyeIcon>
           </div>
         </div>
       </div>
